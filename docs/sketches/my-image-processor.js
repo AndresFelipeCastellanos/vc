c87d64ebe63c1b9c73446ab0b5bf73d6151889d0
AndresFelipeCastellanos <45008613+AndresFelipeCastellanos@users.noreply.github.com>
2021-03-14 15:43:17 +0000

    Deploying to gh-pages from  @ a7ad935dd54fc7c7353c41b29e6300da8905a308 🚀

---
+++ docs/sketches/my-image-processor.js
@@ -0,0 +1,43 @@
+let baseImg;
+let width = 400;
+let height = 300;
+
+function preload(){
+    baseImg = loadImage("/vc/docs/sketches/monte-fuji.jpg","");
+}
+
+function setup(){
+    createCanvas(800,600);
+    image(baseImg, 0, 0, width, height);
+    toNegative(baseImg, 400, 0, width, height);
+    toGray(baseImg, 0, 300, width, height);
+}
+
+/*Set the inverted colors for an image*/
+function toNegative(img, posX, posY, width, height){
+    img.loadPixels();
+    for (let i = 0; i < 4 * img.width * img.height; i += 4) {
+        img.pixels[i] = 255 - img.pixels[i];
+        img.pixels[i + 1] = 255 - img.pixels[i + 1];
+        img.pixels[i + 2] = 255 - img.pixels[i + 2];
+        img.pixels[i + 3] = img.pixels[i + 3];
+    }
+    img.updatePixels();
+    image(img,posX,posY,width,height);
+};
+
+/*Set the grayscale for an image*/
+function toGray(img, posX, posY, width, height){
+    img.loadPixels();
+    let grayColor;
+    for (let i = 0; i < 4 * img.width * img.height; i += 4) {
+        //With the same weight for red, green and blue colors, get the mean of them and set the gray value
+        grayColor = (img.pixels[i] + img.pixels[i + 1] + img.pixels[i + 2])/3;
+        img.pixels[i] = grayColor;
+        img.pixels[i + 1] = grayColor;
+        img.pixels[i + 2] = grayColor;
+        img.pixels[i + 3] = img.pixels[i + 3];
+    }
+    img.updatePixels();
+    image(img,posX,posY,width,height);
+};
